74dca6a8415c922e747609adc07bc13dd593d397
blacktree-lab <19513118+blacktree-lab@users.noreply.github.com>
2025-10-17 15:56:47 +1100

    Delete bundle-main directory

--- bundle-main/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import bundle_raw_mat
-from . import product_template
--- bundle-main/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import bundle_raw_mat_wiz
-from . import add_bundle_product
